ca08cff474457fc08f98bbf09f556bee60af329f
esmeryjose <esmerycornielle@gmail.com>
2018-03-23 22:30:58 -0400

    added comments about next steps

--- src/components/EventShow.js
+++ src/components/EventShow.js
@@ -3,8 +3,24 @@ import { connect } from "react-redux";
 import { getEvent } from "../actions/EventsActions";
 import { IsEmpty } from "../deliverables/IsEmpty";
 import EventBanner from "../components/EventBanner";
+import TextField from "material-ui/TextField";
 
 class EventShow extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchSong: ""
+        };
+    }
+
+    handleChange = e => {
+        const { value } = e.target;
+        this.setState({ searchSong: value });
+        // Send the state to the action
+        // send the request to my api
+        // my api gets data from spotify and that data gets send to the reducer
+    };
+
     componentDidMount() {
         const ids = this.props.match.url.match(/\d+/g);
         this.props.getEvent(ids[0], ids[1]);
@@ -17,12 +33,16 @@ class EventShow extends Component {
     renderEventDetail = () => {
         const { event } = this.props;
         const { admin } = event;
-        console.log(event);
 
         if (!IsEmpty(event)) {
             return (
                 <div>
                     <EventBanner />
+                    <TextField
+                        onChange={this.handleChange}
+                        hintText=""
+                        floatingLabelText="Search for song..."
+                    />
                 </div>
             );
         }
@@ -31,6 +51,8 @@ class EventShow extends Component {
     };
 
     render() {
+        console.log(this.state);
+
         return <div>{this.renderEventDetail()}</div>;
     }
 }
