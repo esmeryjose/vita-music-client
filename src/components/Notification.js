37de29ff242c474ea42a29e3e8050186ab8f9b6c
esmeryjose <esmerycornielle@gmail.com>
2018-06-03 23:39:14 -0400

    notification logo and color added

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,7 +1,6 @@
 import React from "react";
+import Notifications from "material-ui/svg-icons/social/notifications";
 
-const Notification = () => {
-    return <div>Notification</div>;
-};
+const Notification = () => <Notifications style={{ color: "#6bc023" }} />;
 
 export default Notification;
